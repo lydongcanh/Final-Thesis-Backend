d8c2b652372d3505ea0966ad98ed40c47f53803a
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-02-04 16:50:52 +0700

    Add initial migration.

---
+++ FinalThesisBackend/Core/Entities/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalThesisBackend.Core.Entities
+{
+    public class Category
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category ParentCategory { get; set; }
+        public List<Category> ChildrenCategories { get; set; }
+
+        public bool IsRoot
+        {
+            get
+            {
+                return ParentCategory == null;
+            }
+        }
+
+        public bool IsLeaf
+        {
+            get
+            {
+                return ChildrenCategories == null;
+            }
+        }
+    }
+}
---
+++ FinalThesisBackend/Infrastructure/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FinalThesisBackend.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalThesisBackend.Infrastructure
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Category> Categories { get; protected set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Server=localhost,1433;Database=FinalThesisDB;User Id=SA;Password=Thehulk1";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
