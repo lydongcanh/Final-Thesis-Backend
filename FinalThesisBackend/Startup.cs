dbf233b00354912ebf38a00aee3cdaab317d9788
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-02-06 00:08:52 +0700

    Use newtonsoft json to prevent self ref loop issue.

--- FinalThesisBackend/Startup.cs
+++ FinalThesisBackend/Startup.cs
@@ -10,8 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using FinalThesisBackend.Core.Entities;
+using FinalThesisBackend.Core.Interfaces;
 using FinalThesisBackend.Infrastructure;
 
 namespace FinalThesisBackend
@@ -28,7 +30,9 @@ namespace FinalThesisBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(
+                options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore); // Prevent self reference loop issue.
+
 
             services.AddCors(options => options.AddPolicy("DevPolicy", builder =>
             {
@@ -40,6 +44,8 @@ namespace FinalThesisBackend
 
             services.AddDbContext<DataContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("LDCMacDockerConnection")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(DataAsyncRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
