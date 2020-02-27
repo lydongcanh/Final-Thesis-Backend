c369d0bc0b327a9f6222aa26083b17650403f395
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-02-27 22:20:00 +0700

    Add image path to Employee.

--- FinalThesisBackend/Core/Entities/Employee.cs
+++ FinalThesisBackend/Core/Entities/Employee.cs
@@ -14,6 +14,7 @@ namespace FinalThesisBackend.Core.Entities
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string JobTitle { get; set; }
+        public string ImagePath { get; set; }
 
         public string AccountId { get; set; }
         public Account Account { get; set; }
---
+++ FinalThesisBackend/Migrations/20200227151725_ADD_IMAGE_PATH_TO_EMPLOYEE.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalThesisBackend.Migrations
+{
+    public partial class ADD_IMAGE_PATH_TO_EMPLOYEE : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Employees",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Employees");
+        }
+    }
+}
