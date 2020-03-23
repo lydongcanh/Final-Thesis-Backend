ab276b4cc8050a85db7fa2bd508bf8ae4ec59bd9
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-03-24 00:53:44 +0700

    Add Product controller.

---
+++ FinalThesisBackend/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FinalThesisBackend.Core.Entities;
+using FinalThesisBackend.Core.Interfaces;
+
+namespace FinalThesisBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : BaseController<Product>
+    {
+        public ProductsController(IAsyncRepository<Product> repository) : base(repository) { }
+
+        /// <summary>
+        /// GET apis/products.
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            [FromQuery]string containsName = null,
+            [FromQuery]bool? isSelling = null,
+            [FromQuery]string categoryId = null)
+        {
+            if (containsName == null && isSelling == null && categoryId == null)
+                return Ok(await Repository.SelectAllAsync());
+
+            return Ok(await Repository.SelectAsync(p =>
+            {
+                return (containsName == null || p.Name.Contains(containsName)) &&
+                       (isSelling == null || p.IsSelling == isSelling) &&
+                       (categoryId == null || p.CategoryId == categoryId);
+            }));
+        }
+    }
+}
