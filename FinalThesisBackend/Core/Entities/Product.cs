6343bce223584aba5a474a65ac9a39109dff9765
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-03-23 23:40:20 +0700

    Add Product entity.

---
+++ FinalThesisBackend/Core/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalThesisBackend.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public float UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public bool IsSelling { get; set; }
+        public string MainImage { get; set; }
+        public string[] SubImages { get; set; }
+
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
