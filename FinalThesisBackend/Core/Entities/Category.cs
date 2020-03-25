7eff1073f976c616d93a4a8c059f373664b76ab5
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-03-26 00:24:20 +0700

    Add image path to Category entity.

--- FinalThesisBackend/Core/Entities/Category.cs
+++ FinalThesisBackend/Core/Entities/Category.cs
@@ -7,6 +7,7 @@ namespace FinalThesisBackend.Core.Entities
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ImagePath { get; set; }
 
         public string ParentCategoryId { get; set; }
         public Category ParentCategory { get; set; }
---
+++ FinalThesisBackend/Migrations/20200325171802_ADD_IMAGEPATH_TO_CATEGORY.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalThesisBackend.Migrations
+{
+    public partial class ADD_IMAGEPATH_TO_CATEGORY : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Categories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Categories");
+        }
+    }
+}
