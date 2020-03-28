4e0a08d6db880f3c781b97897a882b6e3902dd5c
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-03-28 21:08:02 +0700

    Add CustomerCartItem controller.

---
+++ FinalThesisBackend/Controllers/CustomerCartItemsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FinalThesisBackend.Core.Entities;
+using FinalThesisBackend.Core.Interfaces;
+
+namespace FinalThesisBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerCartItemsController : BaseController<CustomerCartItem>
+    {
+        public CustomerCartItemsController(IAsyncRepository<CustomerCartItem> repository) : base(repository) { }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(
+                [FromQuery]string customerId = null,
+                [FromQuery]string productDetailsId = null)
+        {
+            if (customerId == null && productDetailsId == null)
+                return Ok(await Repository.SelectAllAsync());
+
+            return Ok(await Repository.SelectAsync(cci =>
+            {
+                return (customerId == null || cci.CustomerId == customerId) &&
+                       (productDetailsId == null || cci.ProductDetailsId == productDetailsId);
+            }));
+        }
+    }
+}
--- FinalThesisBackend/Core/Entities/CustomerCartItem.cs
+++ FinalThesisBackend/Core/Entities/CustomerCartItem.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace FinalThesisBackend.Core.Entities
 {
     public class CustomerCartItem : BaseEntity
     {
-        public override string Id
-        {
-            get
-            {
-                return JsonConvert.SerializeObject(new { CustomerId, ProductDetailsId });
-            }
-        }
+        public override string Id { get { return CustomerId + ProductDetailsId; } }
 
         public int Quantity { get; set; }
         public DateTime AddedDate { get; set; }
